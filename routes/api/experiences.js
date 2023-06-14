d00687c259641a585918c3083a82a8e7a298dd32
SB379 <sidbalaji03@gmail.com>
2023-06-14 18:07:21 -0400

    new website lol

--- routes/api/experiences.js
+++ routes/api/experiences.js
@@ -26,13 +26,13 @@ router.get('/test', (req,res) => res.send('experience route testing'));
 const axios = require('axios');
 
 router.get('/', async (req, res) => {
-  try {
+  // try {
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [
         {
           role: "user",
-          content: `Can you provide me with a singular array containing queries for the Google Maps Places API? I would like the array to include itineraries of things I can do in the next two hours, in the format [category1, category2, category3]. For example, [museum, thai food, speakeasy].`
+          content: `What are the top 5 different types of bars that exist?`
         }
       ]
     });
@@ -41,63 +41,63 @@ router.get('/', async (req, res) => {
 
     console.log(query);
 
-    const regex = /\[([^\[\]]*)\]/g;
-    const matches = query.match(regex);
+  //   const regex = /\[([^\[\]]*)\]/g;
+  //   const matches = query.match(regex);
 
-    const itinerary = matches[0].slice(1, -1).split(',').map(item => item.trim());
+  //   const itinerary = matches[0].slice(1, -1).split(',').map(item => item.trim());
 
-    // Function to call Google Places API and return a single result for each itinerary item
-    const getPlaces = async (itinerary) => {
-      const params = {
-        location: `${JSON.parse(req.query.location).coords.latitude},${JSON.parse(req.query.location).coords.longitude}`,
-        radius: 450,
-        key: process.env.GOOGLE_API_KEY
-      };
+  //   // Function to call Google Places API and return a single result for each itinerary item
+  //   const getPlaces = async (itinerary) => {
+  //     const params = {
+  //       location: `${JSON.parse(req.query.location).coords.latitude},${JSON.parse(req.query.location).coords.longitude}`,
+  //       radius: 450,
+  //       key: process.env.GOOGLE_API_KEY
+  //     };
 
-      const results = [];
-      for (const item of itinerary) {
-        const query = item.trim();
+  //     const results = [];
+  //     for (const item of itinerary) {
+  //       const query = item.trim();
 
-        params.query = query;
-        const response = await axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json', { params });
-        const place = response.data.results[0];
+  //       params.query = query;
+  //       const response = await axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json', { params });
+  //       const place = response.data.results[0];
 
-        if (place) {
-          const addressComponents = place.formatted_address.split(", ");
-          const address = {
-            street: addressComponents[0],
-            city: addressComponents[1],
-            state: addressComponents[2].split(" ")[0],
-            zip: addressComponents[2].split(" ")[1],
-          };
+  //       if (place) {
+  //         const addressComponents = place.formatted_address.split(", ");
+  //         const address = {
+  //           street: addressComponents[0],
+  //           city: addressComponents[1],
+  //           state: addressComponents[2].split(" ")[0],
+  //           zip: addressComponents[2].split(" ")[1],
+  //         };
 
-          results.push({
-            name: place.name,
-            address: address,
-            url: place.url,
-            image_url: place.photos ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${place.photos[0].photo_reference}&key=${process.env.GOOGLE_API_KEY}` : null
-            // Add any other desired fields
-          });
-        }
-      }
+  //         results.push({
+  //           name: place.name,
+  //           address: address,
+  //           url: place.url,
+  //           image_url: place.photos ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${place.photos[0].photo_reference}&key=${process.env.GOOGLE_API_KEY}` : null
+  //           // Add any other desired fields
+  //         });
+  //       }
+  //     }
 
-      return results;
-    };
+  //     return results;
+  //   };
 
-    // Call Google Places API for the itinerary
-    const results = await getPlaces(itinerary);
+  //   // Call Google Places API for the itinerary
+  //   const results = await getPlaces(itinerary);
 
-    // Create the response object
-    const responseObject = {
-      itinerary: results,
-      matches: itinerary
-    };
+  //   // Create the response object
+  //   const responseObject = {
+  //     itinerary: results,
+  //     matches: itinerary
+  //   };
 
-    res.json(responseObject);
-  } catch (error) {
-    console.log("Error from Experiences Screen:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
+  //   res.json(responseObject);
+  // } catch (error) {
+  //   console.log("Error from Experiences Screen:", error);
+  //   res.status(500).json({ error: "Internal server error" });
+  // }
 });
 
 
@@ -125,6 +125,102 @@ router.get('/name', async (req, res) => {
     res.status(500).json({error: "Internal server error"});
   }
 });
+
+const getPlaces = async (query, location) => {
+  const params = {
+    location: `${location.lat},${location.long}`,
+    radius: 10000,
+    key: process.env.GOOGLE_API_KEY,
+    query: query.trim() // Set the query parameter
+  };
+
+  const response = await axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json', { params });
+  const places = response.data.results.slice(0, 3); // Get the first three places
+
+  const results = places.map((place) => {
+    const addressComponents = place.formatted_address.split(", ");
+    const address = {
+      street: addressComponents[0],
+      city: addressComponents[1],
+      state: addressComponents[2].split(" ")[0],
+      zip: addressComponents[2].split(" ")[1],
+    };
+
+    return {
+      name: place.name,
+      address: address,
+      url: place.url,
+      image_url: place.photos ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${place.photos[0].photo_reference}&key=${process.env.GOOGLE_API_KEY}` : null
+      // Add any other desired fields
+    };
+  });
+
+  return results;
+};
+
+
+
+
+router.get('/itinerary', async (req, res) => {
+  
+  const { city, query } = req.query;
+
+  try {
+    let lat = "40.7128";
+    let long = "-74.0060";
+
+    const trimmedCity = city.replace(/"/g, "");
+
+    if (trimmedCity === "New York City") {
+      lat = "40.7128";
+      long = "-74.0060";
+    } else if (trimmedCity === "Boston") {
+      lat = "42.3601";
+      long = "-71.0589";
+    } else if (trimmedCity === "Philadelphia") {
+      lat = "39.9526";
+      long = "-75.1652";
+    } else if (trimmedCity === "D.C.") {
+      lat = "38.9072";
+      long = "-77.0379";
+    } else if (trimmedCity === "San Francisco") {
+      lat = "37.7749";
+      long = "-122.4194";
+    } else if (trimmedCity === "San Diego") {
+      lat = "32.7157";
+      long = "-117.1611";
+    } else {
+      console.log("terminated here");
+      return res.status(400).json({ error: "Invalid city" });
+    }
+
+    const location = { lat, long };
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Give me something to do given this information. ${query}. What should I search for on Google Maps?`,
+        },
+      ],
+    });
+
+    const regex = /"([^"]*)"/;
+    const queryFromResponse = response.data.choices[0].message.content.match(regex)[1];
+
+    const places = await getPlaces(queryFromResponse, location);
+
+    console.log(places);
+
+    res.json({ places });
+
+  } catch (error) {
+    console.log("Error from Itinerary Screen: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 
 
 
